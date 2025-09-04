74912863ffdf51d10c30dfe0a62c32ddb6cc9285
mpendulom <mpendulomalangwane@yahoo.com>
2025-09-04 21:10:55 +0200

    init

---
+++ src/test/java/za/co/uride/uride_eureka_server/UrideEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package za.co.uride.uride_eureka_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrideEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
